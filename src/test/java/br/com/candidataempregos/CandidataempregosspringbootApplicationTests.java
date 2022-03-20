640f72345d45c3123ff794ef67a170a53ba7ac44
Érick Solon <erick.solon.prog@gmail.com>
2022-03-20 00:43:35 -0300

    adicionado estrutura da framework spring boot

---
+++ src/test/java/br/com/candidataempregos/CandidataempregosspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.candidataempregos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidataempregosspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
